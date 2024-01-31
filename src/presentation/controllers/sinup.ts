f891e99541f5400fe5dfa7724e47bc4777a9986a
Sara Gomes <sarah.gomes80@hotmail.com>
2024-01-31 09:21:47 -0300

    refactor: remove code duplication

--- src/presentation/controllers/sinup.ts
+++ src/presentation/controllers/sinup.ts
@@ -18,12 +18,12 @@ export class SignUpController implements Controller {
         }
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
-
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
