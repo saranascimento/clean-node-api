2214516fed977d11f2600875091fa506441d23cc
Sara Gomes <sarah.gomes80@hotmail.com>
2024-02-20 08:30:21 -0300

    fix: jest config imports

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require('..jest.config')
+const config = require('./jest.config')
 config.testMatch = ['**/*.test.js']
 module.exports = config
--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require('..jest.config')
+const config = require('./jest.config')
 config.testMatch = ['**/*.spec.js']
 module.exports = config
